1ad6260c4cee617c37cd4f19c8f16dd607cc80f2
Andrew1369 <157983860+Andrew1369@users.noreply.github.com>
2024-11-16 18:31:27 +0200

    Добавив ще два unit тести

--- TestParser/UnitTest1.cs
+++ TestParser/UnitTest1.cs
@@ -51,52 +51,5 @@ namespace TestParser
         }
     }
 
-    public class Parser_Errors
-    {
-        [Fact]
-        public void Calculated_EmptyExpression_ReturnsErrorMessage()
-        {
-            // Arrange
-            Parser parser = new Parser(5, 5);
-            int col = 3;
-            int row = 3;
-            string expression = ""; // Валідний математичний вираз
-
-            // Act
-            string result = parser.Calculated(col, row, expression);
-
-            // Assert
-            Assert.Equal("", result); // Очікуємо результат ""
-        }
-        [Fact]
-        public void Calculated_InvalidExpression_ReturnsErrorMessage()
-        {
-            // Arrange
-            Parser parser = new Parser(8, 5);
-            int col = 1;
-            int row = 1;
-            string expression = "15 + *0 > 3^(1+2)"; // Валідний математичний вираз
-
-            // Act
-            string result = parser.Calculated(col, row, expression);
-
-            // Assert
-            Assert.Equal("SYNTAX", result); // Очікуємо результат SYNTAX
-        }
-        [Fact]
-        public void Calculated_DivisionByZero_ReturnsErrorMessage()
-        {
-            // Arrange
-            Parser parser = new Parser(5, 5);
-            int col = 2;
-            int row = 4;
-            string expression = "5/0"; // Валідний математичний вираз
-
-            // Act
-            string result = parser.Calculated(col, row, expression);
-
-            // Assert
-            Assert.Equal("DIVBYZERO", result); // Очікуємо результат DIVBYZERO
-        }
-    }
+    
 }
---
+++ TestParser/UnitTest3.cs
@@ -0,0 +1,73 @@
+﻿using ParserSpace;
+
+namespace TestParser
+{
+    public class Parser_Address
+    {
+        [Fact]
+        public void Calculated_BoolAddress_ReturnsCorrectResult()
+        {
+            // Arrange
+            Parser parser = new Parser(5, 5);
+            int col = 3;
+            int row = 3;
+            string expression = "4>5"; 
+            parser.Calculated(col, row, expression); // Утворюємо булеву клітинку
+            col = 1;
+            row = 1;
+            expression = "D4"; // Валідний математичний вираз
+
+            // Act
+            string result = parser.Calculated(col, row, expression);
+
+            // Assert
+            Assert.Equal("false", result); // Очікуємо результат false
+        }
+        [Fact]
+        public void Calculated_BoolsAddress_ReturnsCorrectResult()
+        {
+            // Arrange
+            Parser parser = new Parser(8, 5);
+            int col = 1;
+            int row = 0;
+            string expression = "15 + 3";
+            parser.Calculated(col, row, expression); // Утворюємо в клітинці числове значення
+            col = 0;
+            row = 0;
+            expression = "1 + 4*2";
+            parser.Calculated(col, row, expression); // Утворюємо в клітинці числове значення
+            col = 0;
+            row = 1;
+            expression = "A1*3 - B1"; // Валідний математичний вираз
+
+            // Act
+            string result = parser.Calculated(col, row, expression);
+
+            // Assert
+            Assert.Equal("9", result); // Очікуємо результат 9
+        }
+        [Fact]
+        public void Calculated_BoolAddress_ReturnsErrorMessage()
+        {
+            // Arrange
+            Parser parser = new Parser(8, 5);
+            int col = 1;
+            int row = 0;
+            string expression = "15 + 3";
+            parser.Calculated(col, row, expression); // Утворюємо в клітинці числове значення
+            col = 0;
+            row = 0;
+            expression = "1 > 4*2";
+            parser.Calculated(col, row, expression); // Утворюємо в клітинці булеве значення
+            col = 0;
+            row = 1;
+            expression = "A1*3 - B1"; // Валідний математичний вираз
+
+            // Act
+            string result = parser.Calculated(col, row, expression);
+
+            // Assert
+            Assert.Equal("SYNTAX", result); // Очікуємо результат SYNTAX
+        }
+    }
+}
